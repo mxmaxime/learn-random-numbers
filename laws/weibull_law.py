7902cee566210f02dd547d186979cc74a566a527
Maxime Moreau <contact@maxime-moreau.fr>
2020-04-20 10:24:31 +0200

    add(ipynb for weibull law)

--- laws/weibull_law.py
+++ laws/weibull_law.py
@@ -1,3 +1,6 @@
+import numpy as np
+from typing import List
+
 """
 Generate Weibull law from given uniform values.
 """
@@ -6,3 +9,9 @@ def generate_weibull_law(alpha: float, beta: float, uniform_values: List[float])
     b = 1 / beta
 
     return a / b
+
+def weibull_cdf(alpha: float, beta: float, x: float) -> float:
+    return 1 - np.exp(-(alpha * x) ** beta)
+
+def weibull_probability(alpha: float, beta: float, a: float, b: float) -> float:
+    return weibull_cdf(alpha, beta, b) - weibull_cdf(alpha, beta, a)
