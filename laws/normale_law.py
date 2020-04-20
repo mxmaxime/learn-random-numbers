5bd8452498ecfe1e1d34f31366483e7d0cb3ab60
Maxime Moreau <contact@maxime-moreau.fr>
2020-04-20 09:13:56 +0200

    doc

--- laws/normale_law.py
+++ laws/normale_law.py
@@ -1,9 +1,12 @@
+from scipy.stats import norm
 import numpy as np
 from typing import List
 
 
 """
-# génération de deux v.a indépendantes, uniformes sur [0;1]
+Génération de deux v.a normales à partir de deux v.a indépendantes uniformes.
+Utilisation de la méthode de Box et Müller.
+Obtention d'une variable centrée réduite.
 """
 def generate_normale_law(uniform_values1: List[float], uniform_values2: List[float]):
     U = uniform_values1
@@ -16,3 +19,9 @@ def generate_normale_law(uniform_values1: List[float], uniform_values2: List[flo
     Y = np.sqrt(R) * np.sin(theta)
 
     return X, Y
+
+"""
+Probabilité([a,b]) d'une loi normale.
+"""
+def normale_probability(mu: float, sigma: float, a: float, b: float) -> float:
+    return norm.cdf(b, mu, sigma) - norm.cdf(a, mu, sigma)
