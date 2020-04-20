e628d27e26913ce3d003b4bc3a3ce017a89fccff
Maxime Moreau <contact@maxime-moreau.fr>
2020-04-20 10:36:04 +0200

    add(ipynb for poisson law)

--- laws/poisson_law.py
+++ laws/poisson_law.py
@@ -1,4 +1,16 @@
 import numpy as np
+import scipy
+
+"""
+"""
+def generate_poisson_law(lamb: float, size: int):
+    return scipy.stats.poisson.rvs(lamb, size=size)
 
 def poisson(lamb: float, k: int) -> float:
     return (lamb**k) * np.exp(-lamb) / math.factorial(k)
+
+"""
+Probability([a;b])
+"""
+def poisson_probability(lamb: float, a: float, b: float):
+    return scipy.stats.poisson.cdf(b, lamb) - scipy.stats.poisson.cdf(a, lamb)
