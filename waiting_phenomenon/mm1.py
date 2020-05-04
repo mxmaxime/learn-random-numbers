b7b7854084db6d3068660a5bf361d9c26dbf9f07
Maxime Moreau <contact@maxime-moreau.fr>
2020-05-04 22:15:46 +0200

    add(mm1 system): mean formulas

---
+++ waiting_phenomenon/mm1.py
@@ -0,0 +1,32 @@
+"""
+nbs = nb de clients dans le système
+"""
+def e_nbs(psi: float) -> float:
+    return psi / (1-psi)
+
+
+"""
+tas = temps d'attente dans le système
+"""
+def e_tas(psi: float, alpha: float) -> float:
+    return psi / (alpha * (1 - psi))
+
+
+"""
+nbf = nombre de clients dans la file d'attente
+"""
+def e_nbf(psi: float) -> float:
+    return (psi ** 2) / (1-psi)
+
+
+"""
+taf = temps d'attente dans la fille
+"""
+def e_taf(psi: float, alpha: float) -> float:
+    return (psi**2) / (alpha * (1 - psi))
+
+"""
+nbsi = nombre de stations inocupées
+"""
+def e_nbsi(psi: float) -> float:
+    return 1 - psi
